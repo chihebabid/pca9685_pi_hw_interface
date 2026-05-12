4138557a3cfeb3a0180a9292dba09d4b65b54ba0
abid <chiheb.abid@gmail.com>
2026-05-13 00:30:13 +0100

    Add joint configuration and parameter handling in PCA9685 hardware interface

--- src/pca9685_pi_hw_controller/include/pca9685_pi_hw_controller/pca9685_pi_hw_interface.h
+++ src/pca9685_pi_hw_controller/include/pca9685_pi_hw_controller/pca9685_pi_hw_interface.h
@@ -20,6 +20,21 @@ namespace rpi_pca9685_hw_controller {
 
         hardware_interface::return_type write(const rclcpp::Time &time, const rclcpp::Duration &period) override;
     private:
+        struct JointConfig {
+            std::string name;
+            uint8_t channel;
+            uint8_t min_angle {};
+            uint8_t max_angle {180};
+            uint16_t min_pulse_us {1000};
+            uint16_t max_pulse_us {2000};
+        };
+        uint8_t i2c_bus_number_ {1};
+        uint8_t i2c_address_ {0x40};
+        uint8_t pwm_frequency_ {50};
+        std::vector<JointConfig> joint_configs_;
+        std::vector<double> hw_commands_;
+        std::vector<double> hw_states_;
+
         std::unique_ptr<Pca9685Driver> pca9685_driver_;
     };
 }
--- src/pca9685_pi_hw_controller/src/pca9685_pi_hw_interface.cpp
+++ src/pca9685_pi_hw_controller/src/pca9685_pi_hw_interface.cpp
@@ -2,6 +2,7 @@
 // Created by chiheb on 10/05/2026.
 //
 #include "pca9685_pi_hw_controller/pca9685_pi_hw_interface.h"
+#include "rclcpp/rclcpp.hpp"
 
 namespace rpi_pca9685_hw_controller {
 
@@ -11,7 +12,38 @@ namespace rpi_pca9685_hw_controller {
         if (hardware_interface::SystemInterface::on_init(params) != CallbackReturn::SUCCESS) {
             return CallbackReturn::ERROR;
         }
+        if (info_.joints.size()==0 or info_.joints.size() >16) {
+            RCLCPP_ERROR(rclcpp::get_logger("Pca9685PiHwInterface"), "Error : Incorrect number of joints, %zu found!", info_.joints.size());
+            return CallbackReturn::ERROR;
+        }
+
+        for (const auto &param : params.hardware_info.hardware_parameters) {
+            if (param.first == "i2c_bus") {
+                i2c_bus_number_ = std::stoi(param.second);
+            } else if (param.first == "i2c_address") {
+                i2c_address_ = std::stoi(param.second, nullptr, 0);
+            } else if (param.first == "pwm_frequency") {
+                pwm_frequency_ = std::stoi(param.second);
+            }
+        }
+
+        joint_configs_.resize(info_.joints.size());
+        for (size_t i {}; i < info_.joints.size(); ++i) {
+            joint_configs_[i].channel=stoll(info_.joints[i].parameters["channel"]);
+        }
         return CallbackReturn::SUCCESS;
+    }
+
+    std::vector<hardware_interface::StateInterface> Pca9685PiHwInterface::export_state_interfaces() {
+        RCLCPP_DEBUG(rclcpp::get_logger("Pca9685PiHwInterface"), "Export state interfaces");
+        std::vector<hardware_interface::StateInterface> state_interfaces;
+        for (size_t i = 0; i < info_.joints.size(); i++)   {
+
+        }
+
+
+
+        return state_interfaces;
     }
 
 }
